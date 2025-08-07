a77c1bda3cf07f625492ba021a71d7acdc67fdd7
rutu93a <rutujaw06@gmail.com>
2025-08-08 00:18:14 +0530

    This is my first commit

--- Script1.sh
+++ Script1.sh
@@ -1,5 +1,6 @@
 echo "We are trying this practical from linux EC2"
 echo "I am from Young minds"
-echo "We are from batch-27 and learning DevOps"
+echo "We are from batch-29  and learning DevOps"
 echo "We are learning AWS, Azure DevOps and DevSecOps"
 echo "I am learning in batch-29"
+echo "I am practicing Devops"
